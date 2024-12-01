7c1f97226948aaae496ca8b0a08da7aebd6350d8
Michael Baikov <manpacket@gmail.com>
2024-12-01 06:25:40 -0500

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
@@ -22,4 +24,20 @@ pub fn part1(input: &[(u32, u32)]) -> u32 {
     std::iter::zip(a, b)
         .map(|(a, b)| if a > b { a - b } else { b - a })
         .sum()
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &[(u32, u32)]) -> u32 {
+    let mut m = HashMap::<_, u32>::new();
+    for (_, i) in input {
+        *m.entry(*i).or_default() += 1;
+    }
+
+    let mut r = 0;
+
+    for (i, _) in input {
+        r += *i * m.get(i).copied().unwrap_or_default()
+    }
+
+    r
 }
