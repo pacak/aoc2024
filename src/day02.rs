322c2956a2aaa1aa060cbc5c544542980b3f750a
Michael Baikov <manpacket@gmail.com>
2024-12-02 07:57:20 -0500

    Solving it in a smart way was too annoying

--- src/day02.rs
+++ src/day02.rs
@@ -17,6 +17,11 @@ pub fn part1(input: &[Vec<u32>]) -> usize {
     input.iter().filter(|i| safe1(i)).count()
 }
 
+#[aoc(day2, part2)]
+pub fn part2(input: &[Vec<u32>]) -> usize {
+    input.iter().filter(|i| safe2(i)).count()
+}
+
 fn safe1(input: &[u32]) -> bool {
     let dir = input[0].cmp(&input[1]);
     input.windows(2).all(|w| {
@@ -26,4 +31,19 @@ fn safe1(input: &[u32]) -> bool {
         // Any two adjacent levels differ by at least one and at most three.
         a.cmp(&b) == dir && (1..=3).contains(&diff)
     })
+}
+
+/// if violence is not an answer - you are not using enough of it.
+fn safe2(input: &[u32]) -> bool {
+    if safe1(input) {
+        return true;
+    }
+    for i in 0..input.len() {
+        let mut smaller = input.to_vec();
+        smaller.remove(i);
+        if safe1(&smaller) {
+            return true;
+        }
+    }
+    false
 }
