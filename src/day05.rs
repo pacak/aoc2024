8ada3ee7b131eb6aa7d00875e86048cdda848a63
Michael Baikov <manpacket@gmail.com>
2024-12-05 07:04:20 -0500

    Day 5, part 2
    A bubble sort like thing. Horribly ineffficient - looking for reasons to
    swap things and swap.

--- src/day05.rs
+++ src/day05.rs
@@ -29,12 +29,35 @@ fn parse(input: &str) -> (Rules, Sets) {
 fn part1(input: &(Rules, Sets)) -> u32 {
     let mut res = 0;
     for set in input.1.iter() {
-        //        let mut set = set.clone();
         if is_valid_sort(set, &input.0) {
             res += set[set.len() / 2];
         }
     }
     res
+}
+
+#[aoc(day5, part2)]
+fn part2(input: &(Rules, Sets)) -> u32 {
+    let mut res = 0;
+    for set in input.1.iter() {
+        if !is_valid_sort(set, &input.0) {
+            let mut set = set.clone();
+            sort_by_rules(&mut set, &input.0);
+            res += set[set.len() / 2];
+        }
+    }
+    res
+}
+
+// world's most inefficient sort :)
+fn sort_by_rules(input: &mut [u32], rules: &[(u32, u32)]) {
+    for i in 0..input.len() - 1 {
+        for j in i + 1..input.len() {
+            if rules.iter().any(|r| input[j] == r.0 && input[i] == r.1) {
+                input.swap(i, j)
+            }
+        }
+    }
 }
 
 fn is_valid_sort(input: &[u32], rules: &[(u32, u32)]) -> bool {
@@ -82,4 +105,37 @@ fn part1w() {
 61,13,29
 97,13,75,29,47";
     assert_eq!(part1(&parse(input)), 143);
+}
+
+#[test]
+fn part2w() {
+    let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+    assert_eq!(part2(&parse(input)), 123);
 }
