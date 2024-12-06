1a3dcee7dc2f669daabef3708ab81a56373ae91e
Michael Baikov <manpacket@gmail.com>
2024-12-06 08:09:25 -0500

    Day 6, part 1
    Ended up implementing a custom 2d structure for visualization. Wasted
    some time by not including starting position in visited cells.
    
    Prediction for part 2 - some sort of wrap around?

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,69 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
+
+impl std::fmt::Debug for TwoDee<bool> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut col = 0;
+        let mut row = 0;
+        writeln!(f)?;
+        for c in self.data.iter() {
+            if self.poi == (col, row) {
+                write!(f, "X")?;
+            } else {
+                write!(f, "{}", if *c { "#" } else { "." })?;
+            }
+            col += 1;
+            if col == self.width {
+                col = 0;
+                row += 1;
+                writeln!(f)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct TwoDee<T> {
+    width: usize,
+    data: Vec<T>,
+    poi: (usize, usize),
+}
+
+impl<R, T> FromIterator<R> for TwoDee<T>
+where
+    R: Iterator<Item = T>,
+{
+    fn from_iter<I>(i: I) -> Self
+    where
+        I: IntoIterator<Item = R>,
+    {
+        let mut data = Vec::new();
+        let mut width = 0;
+        for (ix, row) in i.into_iter().enumerate() {
+            data.extend(row);
+            if width == 0 {
+                width = data.len();
+            } else {
+                assert_eq!(data.len(), width * (ix + 1));
+            }
+        }
+        Self {
+            data,
+            width,
+            poi: (0, 0),
+        }
+    }
+}
+
+impl<T> TwoDee<T> {
+    fn get(&self, point: (usize, usize)) -> Option<&T> {
+        let (x, y) = point;
+        let i: usize = x + y * self.width;
+        self.data.get(i)
+    }
+}
 
 aoc_lib! { year = 2024 }
