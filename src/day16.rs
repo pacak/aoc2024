3d3fa79e71d8f12bc00a7880eebdcbcf9c5ba95b
Michael Baikov <manpacket@gmail.com>
2024-12-16 08:59:59 -0500

    Day 16, part 2.
    Meh.

--- src/day16.rs
+++ src/day16.rs
@@ -115,10 +115,79 @@ fn part1(input: &(TwoDee<bool>, Point, Point)) -> usize {
     panic!("Where are we?");
 }
 
-// #[aoc(day16, part2)]
-// fn part2(input: &str) -> String {
-//     todo!()
-// }
+#[aoc(day16, part2)]
+fn part2(input: &(TwoDee<bool>, Point, Point)) -> usize {
+    let mut maze = input.0.clone();
+    let start = input.1;
+    let end = input.2;
+
+    let mut q = BinaryHeap::new();
+    q.push(St {
+        score: 0,
+        cur: start,
+        dir: Point::R,
+        visited: [start].into_iter().collect::<HashSet<_>>(),
+    });
+
+    let mut best = HashMap::<(Point, Point), usize>::new();
+    let mut best_path = Vec::<St>::new();
+    while let Some(s) = q.pop() {
+        if s.cur == end {
+            match best_path.last() {
+                Some(p) if p.score < s.score => {
+                    break;
+                }
+                _ => {
+                    best_path.push(s);
+                    continue;
+                }
+            }
+        }
+
+        for d in [Point::U, Point::R, Point::D, Point::L] {
+            if maze[s.cur + d] {
+                // wall that way
+                continue;
+            }
+
+            if s.dir * -1 == d {
+                // don't go back
+                continue;
+            }
+
+            if s.visited.contains(&(s.cur + d)) {
+                continue;
+            }
+
+            maze.poi = (s.cur.x as usize, s.cur.y as usize);
+
+            let mut s = s.clone();
+            s.score += if s.dir == d { 1 } else { 1001 };
+            s.cur = s.cur + d;
+            s.dir = d;
+            s.visited.insert(s.cur);
+            let t = (s.cur, d);
+            let prev = *best.get(&t).unwrap_or(&100000000000000);
+
+            if prev >= s.score {
+                if s.score > prev + 1000 {
+                    continue;
+                }
+                if prev >= s.score {
+                    best.insert(t, s.score);
+                }
+
+                q.push(s);
+            }
+        }
+    }
+
+    best_path
+        .iter()
+        .flat_map(|b| b.visited.iter())
+        .collect::<HashSet<_>>()
+        .len()
+}
 
 #[cfg(test)]
 mod tests {
@@ -165,8 +234,44 @@ mod tests {
         assert_eq!(part1(&parse(input)), 11048);
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+    #[test]
+    fn part2_example() {
+        let input = "\
+###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
+        assert_eq!(part2(&parse(input)), 45);
+
+        let input = "\
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
+        assert_eq!(part2(&parse(input)), 64);
+    }
 }
